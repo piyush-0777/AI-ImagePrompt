49ff26fe8436690b2e5b13157b76bd7f24bf6a9e
piyush-0077 <khandharpiyush5@gmail.com>
2026-07-01 16:58:11 +0530

    add

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,20 +1,12 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT || 5432,
-  database: process.env.DB_NAME,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  ssl: {
-    rejectUnauthorized: true,
-  },
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
 });
-console.log( process.env.DB_HOST,
- process.env.DB_PORT ,
-   process.env.DB_NAME,
-   process.env.DB_USER,
- process.env.DB_PASSWORD,)
+
+console.log('dataBase_url' ,process.env.DATABASE_URL )
+
 pool.on("connect", () => {
   console.log("✅ PostgreSQL Connected");
 });
